74d4a208492b7716d7ea2140c5d41d4b1ebe7bd0
Alexandr Bochev <abochev@gmail.com>
2024-01-19 19:03:37 +0200

    btn

--- src/components/ButtonTransparent/ButtonTransparent.tsx
+++ src/components/ButtonTransparent/ButtonTransparent.tsx
@@ -8,16 +8,9 @@ interface ButtonProps {
   onClick?: () => void
 }
 
-const btnMotion = {
-  initial: {},
-  whileInView: {},
-  transition: { delay: 1, duration: 0.8, ease: "easeInOut" },
-}
-
 const ButtonTransparent = ({ children, arrow, onClick }: ButtonProps) => {
   return (
     <m.button
-      { ...btnMotion }
       className="
         h-12 border-transparent rounded-full backdrop-blur-sm bg-[#000010]/30 px-5.5
         before:absolute before:inset-0 before:z-10
@@ -30,9 +23,13 @@ const ButtonTransparent = ({ children, arrow, onClick }: ButtonProps) => {
       onClick={ onClick }
     >
       <div className="absolute inset-0 w-full h-full rounded-full opacity-0 group-hover:opacity-100 bg-gradient-to-t from-white/20 to-black/0 transition-all duration-1000" />
-      <div className={`flex items-center text-nowrap ${FONT_STYLES_SILVER}
-      bg-clip-text text-transparent bg-gradient-to-t from-white to-white/40
-      hover:text-white transition-all duration-500`}>
+      <div
+        className={`
+          flex items-center text-nowrap ${FONT_STYLES_SILVER}
+          bg-clip-text text-transparent bg-gradient-to-t from-white to-white/40
+        hover:text-white transition-all duration-500
+        `}
+      >
           { children }
           { arrow && <Arrow /> }
       </div>
